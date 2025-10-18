1a8ddf91bbcc603eca677200fde044ba6af733fa
Aliamzzz <ajangali2000@gmail.com>
2025-10-18 18:22:30 +0330

    Category
    +Introduces a new Category model and associates it with articles via a many-to-many relationship.
    +Adds a published date field to Article
    +Modified views and templates to display categories.
    +Changed the time zone setting to Asia/Tehran.

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,25 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 class Article(models.Model):
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    category = models.ManyToManyField(Category)
     title = models.CharField(max_length=100)
     content = models.TextField()
     image = models.ImageField(upload_to="images/articles")
     pub_date = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    published = models.DateField(default=timezone.now)
 
     def __str__(self):
         return self.title
--- home/views.py
+++ home/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from blog.models import Article
+from blog.models import Article, Category
+
 
 # Create your views here.
 
 def home(request):
     articles = Article.objects.all()
-    return render(request, 'home/index.html', {'articles': articles})
+    categories = Category.objects.all()
+    return render(request, 'home/index.html', {'articles': articles, 'categories': categories})
